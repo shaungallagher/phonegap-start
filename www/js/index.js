02e0b055dd9c9c3cb40ec33bdfdf6aac196a7ab0
Shaun Gallagher <sgallagher@monetate.com>
2013-01-14 09:56:21 -0500

    app.updateDB, app.fromDB, jsdocs

--- www/js/index.js
+++ www/js/index.js
@@ -2,63 +2,144 @@
 
 var app = {};
 
-app.report = function (id) {
-    console.log("Report: " + id);
+
+/**
+ * The device ID.
+ */
+var app.device_id;
+
+
+/**
+ * Output data to both console.log and the DOM.
+ * @param  {string} data The data to output.
+ */
+app.report = function (data) {
+    console.log('Report: ' + data);
     var c = $('#console');
-    var p = $('<p>' + id + '</p>');
+    var p = $('<p>' + data + '</p>');
     $(c).append(p);
 };
 
+
+/**
+ * Wait for device ready before continuing.
+ */
 app.initialize = function () {
     console.log('initialize');
-    alert('initialize');
-    app.bind();
+    document.addEventListener('deviceReady', app.deviceready, false);
 };
 
-app.bind = function () {
-    console.log('bind');
-    alert('bind');
-    document.addEventListener("deviceReady", app.deviceready, false);
-};
 
+/**
+ * Check for existence of device ID.
+ */
 app.deviceready = function () {
     app.report('deviceready');
-    alert('deviceReady');
     if (device && device.uuid) {
         app.report('device.uuid: ' + device.uuid);
+        app.device_id = device.uuid;
         app.login();
     } else {
         app.report('!device.uuid');
     }
 };
 
+
+/**
+ * Use web API to check whether an update is available.
+ */
 app.login = function () {
     app.report('login');
-    alert('login');
-    var device_id = device && device.uuid;
-    var update_id = window.localStorage.getItem("update_id");
+    var update_id = window.localStorage.getItem('update_id') || -1;
     $.ajax({
         url: 'http://www.granniephone.com/api/?action=login&device_id=' +
-            device_id + '&update_id=' + update_id,
+             encodeURIComponent(app.device_id) + '&update_id=' +
+             encodeURIComponent(update_id),
         dataType: 'jsonp',
         timeout: 3000,
         success: function (data, status) {
-            alert('success');
             app.report('login success: ' + data.response);
+            if (data.response != 'up to date') {
+                app.updateDB(data);
+            }
+            app.fromDB();
         },
         error: function () {
-            alert('error');
-            app.report('login error');
+            app.report('API login error');
         }
     });
 };
 
 
+/**
+ * Update the database with the data returned from the web API.
+ * @param  {Object} data Data from the web API.
+ */
+app.updateDB = function (data) {
+
+    var populateDB = function (tx) {
+        tx.executeSql('DROP TABLE IF EXISTS data');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS data ' +
+            '(id integer primary key autoincrement, device_id, item, key, value)');
+
+        var item, properties, j, property;
+        for (var i = 0; i < data.items.length; i++) {
+            item = data.items[i];
+            properties = Object.getOwnPropertyNames(item);
+            for (j = 0; j < properties.length; j++) {
+                property = properties[j];
+                tx.executeSql('INSERT INTO data (device_id, item, key, value) VALUES (?,?,?)',
+                    [app.device_id, i, property, item[property]]);
+            }
+        }
+    };
+
+    var errorDB = function (err) {
+        app.report('app.updateDB - Error processing SQL: ' + err.code);
+    };
+
+    var successDB = function () {
+        app.report('app.updateDB - success!');
+    };
+
+    window.localStorage.setItem('update_id', data.update_id);
+    var db = window.openDatabase('data', '1.0', 'GranniePhone data', 1000000);
+    db.transaction(populateDB, errorDB, successDB);
+
+};
+
+
+/**
+ * Get our settings from the database.
+ */
+app.fromDB = function () {
+
+    var queryDB = function () {
+        tx.executeSql('SELECT * FROM data WHERE device_id = ?', [app.device_id],
+            querySuccess, errorDB);
+    };
+
+    var errorDB = function (err) {
+        app.report('app.fromDB - Error processing SQL: ' + err.code);
+    };
+
+    var querySuccess = function () {
+        app.report('app.fromDB - success!');
+        app.report('Results length: ' + results.rows.length);
+    };
+
+    var db = window.openDatabase('data', '1.0', 'GranniePhone data', 1000000);
+    db.transaction(queryDB, errorDB);
+
+};
+
+
+
 // findBetty: function () {
 //     console.log('findBetty 2');
 //     var options = new ContactFindOptions();
-//     options.filter = "Betty";
-//     var fields = ["displayName", "name"];
+//     options.filter = 'Betty';
+//     var fields = ['displayName', 'name'];
 //     navigator.contacts.find(fields, onSuccess, onError, options);
 // }
 
@@ -67,7 +148,7 @@ app.login = function () {
 //     navigator.notification.alert('onSuccess!', function () {}, 'Title');
 //     for (var i=0; i<contacts.length; i++) {
 //         document.body.appendChild('<p style="background-color:#0F0">' + contacts[i].displayName +
-//             ', <a href="tel:' + contacts[i].phoneNumbers[0] + '</p>');
+//             ', <a href="tel:' + contacts[i].phoneNumbers[0] + '">contacts[i].phoneNumbers[0]</a></p>');
 //     }
 // };
 
